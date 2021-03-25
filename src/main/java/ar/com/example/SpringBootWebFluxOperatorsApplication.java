f7f7692f87b78047625c3f2c90f97352b287ab95
Soria <maximilianorodrigosoria@gmail.com>
2021-03-25 10:14:25 -0300

    doOnNext is added

--- src/main/java/ar/com/example/SpringBootWebFluxOperatorsApplication.java
+++ src/main/java/ar/com/example/SpringBootWebFluxOperatorsApplication.java
@@ -1,13 +1,22 @@
 package ar.com.example;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import reactor.core.publisher.Flux;
 
 @SpringBootApplication
-public class SpringBootWebFluxOperatorsApplication {
+@Slf4j
+public class SpringBootWebFluxOperatorsApplication implements CommandLineRunner {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringBootWebFluxOperatorsApplication.class, args);
     }
 
+    @Override
+    public void run(String... args) throws Exception {
+        Flux<String> nombres = Flux.just("Maximiliano","Marta","Yesica","Luciana","Karen", "Marcelo");
+        nombres.doOnNext(log::info).subscribe();
+    }
 }
