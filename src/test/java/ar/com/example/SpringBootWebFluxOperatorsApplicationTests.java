0da3128748d68732bdc3b3018a4c7eee6ad349fe
Soria <maximilianorodrigosoria@gmail.com>
2021-03-25 09:20:34 -0300

    initial commit

---
+++ src/test/java/ar/com/example/SpringBootWebFluxOperatorsApplicationTests.java
@@ -0,0 +1,13 @@
+package ar.com.example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootWebFluxOperatorsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
